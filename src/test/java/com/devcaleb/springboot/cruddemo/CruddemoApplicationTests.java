3f56e9b765600dec11c1cda91bcebc2d9c0366bf
Marcelo Caleb Júlio Ofice <marcelocalebjulioo@gmail.com>
2025-11-09 23:24:29 +0200

    Project created

---
+++ src/test/java/com/devcaleb/springboot/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcaleb.springboot.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
