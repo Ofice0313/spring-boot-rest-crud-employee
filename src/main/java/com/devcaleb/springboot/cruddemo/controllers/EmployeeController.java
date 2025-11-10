51a6fb3f6a810f016e12fdbad3bf0f28d8635f2a
Marcelo Caleb Júlio Ofice <marcelocalebjulioo@gmail.com>
2025-11-10 10:40:02 +0200

    Implementing the service layer

--- src/main/java/com/devcaleb/springboot/cruddemo/controllers/EmployeeController.java
+++ src/main/java/com/devcaleb/springboot/cruddemo/controllers/EmployeeController.java
@@ -2,6 +2,8 @@ package com.devcaleb.springboot.cruddemo.controllers;
 
 import com.devcaleb.springboot.cruddemo.dao.EmployeeDAO;
 import com.devcaleb.springboot.cruddemo.entities.Employee;
+import com.devcaleb.springboot.cruddemo.services.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,15 +14,16 @@ import java.util.List;
 @RequestMapping("/api")
 public class EmployeeController {
 
-    private EmployeeDAO employeeDAO;
+    private EmployeeService employeeService;
 
-    public EmployeeController(EmployeeDAO theEmployeeDAO) {
-        employeeDAO = theEmployeeDAO;
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
     }
 
     @GetMapping("/employees")
     public List<Employee> findAll() {
-        return employeeDAO.findAll();
+        return employeeService.findAll();
     }
 
 }
--- src/main/java/com/devcaleb/springboot/cruddemo/services/EmployeeService.java
+++ src/main/java/com/devcaleb/springboot/cruddemo/services/EmployeeService.java
@@ -1,4 +1,10 @@
 package com.devcaleb.springboot.cruddemo.services;
 
+import com.devcaleb.springboot.cruddemo.entities.Employee;
+
+import java.util.List;
+
 public interface EmployeeService {
+
+    List<Employee> findAll();
 }
