b41193151cfac1cba4c8e4f9b739dfcb32a87a53
Marcelo Caleb Júlio Ofice <marcelocalebjulioo@gmail.com>
2025-11-10 12:06:03 +0200

    Endpoint POST '/employees

--- src/main/java/com/devcaleb/springboot/cruddemo/controllers/EmployeeController.java
+++ src/main/java/com/devcaleb/springboot/cruddemo/controllers/EmployeeController.java
@@ -4,10 +4,7 @@ import com.devcaleb.springboot.cruddemo.dao.EmployeeDAO;
 import com.devcaleb.springboot.cruddemo.entities.Employee;
 import com.devcaleb.springboot.cruddemo.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -37,4 +34,17 @@ public class EmployeeController {
         }
         return employee;
     }
+
+    // add mapping for POST /employees - add new employee
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee employee){
+
+        // also just in case they pass an id in JSON ... set id to 0
+        // this is to force a  save of new item ... instead of update
+
+        employee.setId(null);
+        Employee dbEmployee = employeeService.save(employee);
+        return dbEmployee;
+    }
+
 }
