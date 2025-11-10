890394a1c32f03155388743910d22bfa7352fefe
Marcelo Caleb Júlio Ofice <marcelocalebjulioo@gmail.com>
2025-11-10 11:54:30 +0200

    Endpoint '/employees/{employeeId}

--- src/main/java/com/devcaleb/springboot/cruddemo/controllers/EmployeeController.java
+++ src/main/java/com/devcaleb/springboot/cruddemo/controllers/EmployeeController.java
@@ -5,6 +5,7 @@ import com.devcaleb.springboot.cruddemo.entities.Employee;
 import com.devcaleb.springboot.cruddemo.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,4 +27,14 @@ public class EmployeeController {
         return employeeService.findAll();
     }
 
+    // add mapping for GET /employees/{employeeId}
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+        Employee employee = employeeService.findById(employeeId);
+
+        if(employee == null) {
+            throw new RuntimeException("Employee id not foundb -> " + employeeId);
+        }
+        return employee;
+    }
 }
