dbe076e49f1d32f747916b45b7dd5b68919869b0
Marcelo Caleb Júlio Ofice <marcelocalebjulioo@gmail.com>
2025-11-12 18:09:14 +0200

    Spring Data REST properties

--- src/main/java/com/devcaleb/springboot/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/devcaleb/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.devcaleb.springboot.cruddemo.dao;
 
 import com.devcaleb.springboot.cruddemo.entities.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
