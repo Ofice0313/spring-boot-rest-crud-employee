279999f5f6cdc6f756f150ec548bd7f9e5cd26bc
Marcelo Caleb Júlio Ofice <marcelocalebjulioo@gmail.com>
2025-11-13 12:35:48 +0200

    Restrict Access Based on Roles

--- src/main/java/com/devcaleb/springboot/cruddemo/security/DemoSecurityConfig.java
+++ src/main/java/com/devcaleb/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -2,9 +2,13 @@ package com.devcaleb.springboot.cruddemo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -31,5 +35,24 @@ public class DemoSecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer ->
+                configurer.requestMatchers(HttpMethod.GET, "/magic-api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/magic-api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/magic-api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/magic-api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/magic-api/employees/**").hasRole("ADMIN")
+        );
+
+        // use HTTP Basic authentication
+        http.httpBasic(Customizer.withDefaults());
+
+        // disable Cross Site Request Forgery (CSRF)
+        // in general, not required for stateless REST API's that use POST, PUT, DELETE and/o PATCH
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
     }
 }
